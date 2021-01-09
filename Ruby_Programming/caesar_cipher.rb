0d6feb4a8bd399dd61096f6d83077b99ae559b12
Mayo <patrick.mayo.moran@gmail.com>
2021-01-09 07:03:49 -0500

    adjust offset to 26 and test with example

--- Ruby_Programming/caesar_cipher.rb
+++ Ruby_Programming/caesar_cipher.rb
@@ -19,9 +19,9 @@ p convert_to_nums("My man!")
 def add_offset(num_array, offset)
   offset_array = num_array.map do |num|
     if lowercase?(num)
-      lower_offset(num, offset)
+      return lower_offset(num, offset)
     elsif  uppercase?(num)
-      upper_offset(num, offset)
+      return upper_offset(num, offset)
     else
       return num.chr
     end
@@ -29,18 +29,18 @@ def add_offset(num_array, offset)
 end
 
 def lowercase?(num)
-  num.between?(97, 122)
+  return num.between?(97, 122)
 end
 
 def uppercase?(num)
-  num.between?(65, 90)
+  return num.between?(65, 90)
 end
 
 def lower_offset(num, offset)
   num -= 97
   position = num + offset
   if position > 25
-    position -= 25
+    position -= 26
   end
 
   alphabet = ('a'..'z').to_a
@@ -51,11 +51,11 @@ def upper_offset(num, offset)
   num -= 65
   position = num + offset
   if position > 25
-    position -= 25
+    position -= 26
   end
 
   alphabet = ('A'..'Z').to_a
   return alphabet[position]
 end
   
-puts make_caesar_cipher("Dirt on your couch!")
+puts make_caesar_cipher("Dirt on your couch!", -6)
